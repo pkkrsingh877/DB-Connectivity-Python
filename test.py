99c98035ab280c2407456b4e5081c2e7ad1daf0e
Prabhat Kumar <pkkrsingh877@gmail.com>
2024-04-23 00:07:56 +0530

    Added Features to list all students, single student, and a cleanup function named exitt

--- test.py
+++ test.py
@@ -26,34 +26,66 @@ print('''
 ''')
 
 def register():
+    print("*"*30)
+    print("REGISTER NEW STUDENT")
+    print("*"*30)
     name = input("Enter Name: ")
     enrollment = input("Enter Enrollment Number: ")
     college = input("Enter College Name: ")
     branch = input("Enter Branch Name: ")
     contact = input("Enter Contact Number: ")
+    print("*"*30)
 
     query = "INSERT INTO student (name, enrollment, college, branch, contact) VALUES (%s, %s, %s, %s, %s)"
     data = (name, enrollment, college, branch, contact)
     cursor.execute(query, data)
     print("Registration Complete...")
-
-    connection.commit()
-    cursor.close()
-    connection.close()
-
-
+    exitt()
 
 def login():
     pass
 
 def listStudents():
-    pass
+    query = "SELECT name, enrollment FROM student;"
+
+    cursor.execute(query)
+
+    rows = cursor.fetchall() # Store previous query's data in rows
+
+    print("*"*30)
+    print("Name\t\t\t\tEnrollement")
+    print("*"*30)
+    for row in rows:
+        name, enrollment = row # Destructuring
+        print(f"{name}\t\t\t{enrollment}")
+    print("*"*30)
+    exitt()
 
 def listStudent():
-    pass
+    enrollment = input('Enter Enrollment Number: ')
+    query = "SELECT * FROM student WHERE enrollment = %s;"
+    
+    cursor.execute(query, (enrollment,))
+
+    row = cursor.fetchone() # Store previous query's data in row
+    if row:
+        name, enrollment, college, branch, contact = row[:5]
+        print("*"*30)
+        print("STUDENT DETAILS")
+        print("*"*30)
+        print(f"Name: {name}\nEnrollment Number: {enrollment}\nCollege Name: {college}\nBranch Name: {branch}\nContact Number: {contact}")
+    else:
+        print("Student with entered Enrollment Number does not exist...")
+    
+    exitt()
 
 def exitt():
+    connection.commit()
+    cursor.close()
+    connection.close()
+    print("*"*30)
     print("Exitting The Program...")
+    print("*"*30)
     exit()
 
 choice = input("What Operation do you want to perform: ")
